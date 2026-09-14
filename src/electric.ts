38b3d4bd5617a33bd49fcc1de102598264c746f1
ATTHENDRE <albert-toth.endre@petrik.hu>
2026-09-14 10:39:09 +0200

    sdlk fjksbhfhjkbn

---
+++ src/electric.ts
@@ -0,0 +1,13 @@
+export class ElectricRat{
+     
+    public name : string
+    public atk : number
+    public hp : number
+
+
+    public constructor(name: string, atk: number, hp: number){
+        this.name = name;
+        this.atk = atk;
+        this.hp = hp;
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,54 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.ts'
+import { ElectricRat } from "./electric"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById("ujform")?.addEventListener('submit', Uj)
+})
 
-<div class="ticks"></div>
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+const adatok : ElectricRat[] = []
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+function Uj(e:SubmitEvent){
+    e.preventDefault();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    const ujnev = document.getElementById("nev") as HTMLInputElement
+
+    const ujPatkany = new ElectricRat(
+        ujnev.value,
+        Math.floor(Math.random()*11) + 10, 
+        Math.floor(Math.random()*51) + 50 
+    )
+
+    adatok.push(ujPatkany)
+
+
+    loadData()
+    
+
+}
+
+function loadData(){
+    const lista = document.getElementById("kartyak")
+
+    for(const item of adatok){
+        const div = document.createElement("div")
+        
+
+        const nev = document.createElement("h2")
+        nev.textContent = item.name
+        div.appendChild(nev)
+
+        const atk = document.createElement("p")
+        atk.textContent = item.atk.toString()
+        div.appendChild(atk)
+
+        const hp = document.createElement("p")
+        hp.textContent = item.hp.toString()
+        div.appendChild(hp)
+
+
+        lista?.appendChild(div)
+
+    }
+
+
+}
